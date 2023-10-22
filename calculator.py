c9db3dd55624a5e03fa942e58122f7ddbdf0e2fb
Kasun_Udayanga <35797707+KasunUdayanga@users.noreply.github.com>
2023-10-22 14:52:48 +0530

    Update calculator.py
    add new method

--- calculator.py
+++ calculator.py
@@ -1,73 +1,92 @@
-def add(a, b):
-    return a + b
+def add(a,b):
+  return a+b
+  
+def subtract(a,b):
+  return a-b
+  
+def multiply (a,b):
+  return a*b
 
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    if b == 0:
-        return "float division by zero"
-    return a / b
-
-def power(a, b):
-    return a ** b
-
-def remainder(a, b):
-    if b == 0:
-        return "float modulo by zero"
-    return a % b
+def divide(a,b):
+  try:
+    return a/b
+  except Exception as e:
+    print(e)
+def power(a,b):
+  return a**b
+  
+def remainder(a,b):
+  return a%b
 
 def select_op(choice):
-    if choice == '+':
-        return add
-    elif choice == '-':
-        return subtract
-    elif choice == '*':
-        return multiply
-    elif choice == '/':
-        return divide
-    elif choice == '^':
-        return power
-    elif choice == '%':
-        return remainder
-    else:
-        return None
-
-while True:
-    print("Select operation.")
-    print("1.Add      : +")
-    print("2.Subtract : -")
-    print("3.Multiply : *")
-    print("4.Divide   : /")
-    print("5.Power    : ^")
-    print("6.Remainder: %")
-    print("7.Terminate: #")
-    print("8.Reset    : $")
-
-    choice = input("Enter choice(+,-,*,/,^,%,#,$):")
-
-    if choice == '#':
-        print("Done. Terminating")
+  if (choice == '#'):
+    return -1
+  elif (choice == '$'):
+      return 0
+  elif (choice in ('+','-','*','/','^','%')):
+    while (True):
+      num1s = str(input("Enter first number: "))
+      print(num1s)
+      if num1s.endswith('$'):
+        return 0
+      if num1s.endswith('#'):
+        return -1
+        
+      try:
+        num1 = float(num1s)
         break
-    elif choice == '$':
+      except:
+        print("Not a valid number,please enter again")
         continue
-    elif choice not in ('+', '-', '*', '/', '^', '%'):
-        print("Unrecognized operation")
+    
+    while (True):
+      num2s = str(input("Enter second number: "))
+      print(num2s)
+      if num2s.endswith('$'):
+        return 0
+      if num2s.endswith('#'):
+        return -1
+      try:  
+        num2 = float(num2s)
+        break
+      except:
+        print("Not a valid number,please enter again")
         continue
-
-    try:
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-    except ValueError:
-        print("Not a valid number, please enter again")
-        continue
-
-    operation = select_op(choice)
-    if operation is not None:
-        result = operation(num1, num2)
-        print(f"{num1} {choice} {num2} = {result}")
+      
+    if choice == '+':
+      print(num1, "+", num2, "=", add(num1, num2))
+  
+    elif choice == '-':
+      print(num1, "-", num2, "=", subtract(num1, num2))
+  
+    elif choice == '*':
+      print(num1, "*", num2, "=", multiply(num1, num2))
+  
+    elif choice == '/':
+      print(num1, "/", num2, "=", divide(num1, num2))
+    elif choice == '^':
+      print(num1, "^", num2, "=", power(num1, num2))
+    elif choice == '%':
+      print(num1, "%", num2, "=", remainder(num1, num2))
     else:
-        print("Unrecognized operation")
+      print("Something Went Wrong")
+  else:
+    print("Unrecognized operation")
+    
+while True:
+  print("Select operation.")
+  print("1.Add      : + ")
+  print("2.Subtract : - ")
+  print("3.Multiply : * ")
+  print("4.Divide   : / ")
+  print("5.Power    : ^ ")
+  print("6.Remainder: % ")
+  print("7.Terminate: # ")
+  print("8.Reset    : $ ")
+  
+
+  choice = input("Enter choice(+,-,*,/,^,%,#,$): ")
+  print(choice)
+  if(select_op(choice) == -1):
+    print("Done. Terminating")
+    exit()
